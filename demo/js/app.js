0288b26d1a2e95310ae99b7f095ebc55716baff6
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-05-20 17:07:27 +0600

    Added stepped progress

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,16 +1,40 @@
-angular.module('app', ['ui.multiStepProgress'])
-    .controller("MainController", function ($scope) {
-        $scope.progressValues = [100, 30];
-        $scope.steps = [
-            {
-                waiting: 'File load',
-                inProgress: 'Loading file ...',
-                done: 'File loaded'
-            },
-            {
-                waiting: 'Validation',
-                inProgress: 'Validating data ...',
-                done: 'Data validated'
+(function () {
+    angular.module('app', ['ui.multiStepProgress'])
+        .controller("MainController", function ($scope, $interval) {
+            $scope.steps = [
+                {
+                    waitingText: 'File load',
+                    inProgressText: 'Loading file ...',
+                    doneText: 'File loaded'
+                },
+                {
+                    waitingText: 'Validation',
+                    inProgressText: 'Validating data ...',
+                    doneText: 'Data validated'
+                },
+                {
+                    waitingText: 'Synchronization',
+                    inProgressText: 'Synchronizing data ...',
+                    doneText: 'Data synchronized'
+                }
+            ];
+            $scope.progressValues1 = [0, 0, 0];
+            $scope.progressValues2 = [0, 0, 0];
+            $scope.progressValues3 = [0, 0, 0];
+            $scope.progressValues4 = [0, 0, 0];
+
+            $scope.start = function (progressValues) {
+                for (var i = 0; i < progressValues.length; i++) {
+                    progressValues[i] = 0;
+                }
+                $interval(function () {
+                    for (var i = 0; i < progressValues.length; i++) {
+                        if (progressValues[i] < 100) {
+                            progressValues[i] += 2;
+                            break;
+                        }
+                    }
+                }, 200)
             }
-        ];
-    });
+        });
+}());
--- src/multi-step-progress.js
+++ src/multi-step-progress.js
@@ -1,59 +1,101 @@
-angular.module('ui.multiStepProgress', [])
-    .directive('multiStepProgress', [
-        function () {
-            var multiStepProgress = function ($scope) {
-                //$scope.progressValues = [100, 30];
-                //$scope.steps = [
-                //    {
-                //        waiting: 'File load',
-                //        inProgress: 'Loading file ...',
-                //        done: 'File loaded'
-                //    },
-                //    {
-                //        waiting: 'Validation',
-                //        inProgress: 'Validating data ...',
-                //        done: 'Data validated'
-                //    }
-                //];
+(function () {
+    angular.module('ui.multiStepProgress', [])
+        .directive('multiStepProgress', [
+            function () {
+                var multiStepProgress = function ($scope) {
+                    //$scope.progressValues = [100, 30];
+                    /*$scope.steps = [
+                        {
+                            waitingText: 'File load',
+                            inProgressText: 'Loading file ...',
+                            doneText: 'File loaded'
+                        },
+                        {
+                            waitingText: 'Validation',
+                            inProgressText: 'Validating data ...',
+                            doneText: 'Data validated'
+                        },
+                        {
+                            waitingText: 'Synchronization',
+                            inProgressText: 'Synchronizing data ...',
+                            doneText: 'Data synchronized'
+                        }
+                    ];*/
 
-                $scope.getStyle = function (index) {
-                    return {
-                        "width": $scope.progressValues[index] + "%"
-                    }
-                };
-                $scope.getLabel = function (index) {
-                    var result = $scope.steps[index].waiting;
-                    if ($scope.progressValues[index] > 0) {
-                        result = $scope.steps[index].inProgress;
-                    }
-                    if ($scope.progressValues[index] >= 100) {
-                        result = $scope.steps[index].done;
-                    }
-                    return result;
-                };
-                $scope.getStatus = function (index) {
-                    var result = 'waiting';
-                    if ($scope.progressValues[index] > 0) {
-                        result = $scope.progressValues[index] + "%";
-                    }
-                    if ($scope.progressValues[index] >= 100) {
-                        result = 'Done';
-                    }
-                    return result;
-                };
-            };
+                    $scope.statuses = [];
 
-            return {
-                controller: [
-                    '$scope',
-                    multiStepProgress
-                ],
-                scope: {
-                    steps: '=',
-                    progressValues: '='
-                },
-                templateUrl: 'multistepprogress.html',
-                replace: true
+                    function init(){
+                        var isAlreadyFound = false;
+                        for(var i=0; i<$scope.steps.length; i++){
+                            if($scope.progressValues[i]==0){
+                                $scope.statuses[i] = isAlreadyFound?"msp-waiting":"msp-in-progress";
+                                isAlreadyFound = true;
+                            } else if($scope.progressValues[i]>=100){
+                                $scope.statuses[i] = "msp-done";
+                            } else {
+                                if(isAlreadyFound){
+                                    $scope.progressValues[i] = 0;
+                                    $scope.statuses[i] = "msp-waiting";
+                                } else{
+                                    $scope.statuses[i] = "msp-in-progress";
+                                    isAlreadyFound = true;
+                                }
+                            }
+                        }
+                    }
+                    $scope.$watchCollection(
+                        "progressValues",
+                        function( newValue, oldValue ) {
+                            init();
+                        }
+                    );
+
+
+
+                    $scope.getStyle = function (index) {
+                        return {
+                            "width": $scope.progressValues[index] + "%"
+                        }
+                    };
+                    $scope.getLabel = function (index) {
+                        var result = $scope.steps[index].waitingText;
+                        if ($scope.progressValues[index] > 0) {
+                            result = $scope.steps[index].waitingText;
+                        }
+                        if ($scope.progressValues[index] >= 100) {
+                            result = $scope.steps[index].waitingText;
+                        }
+                        return result;
+                    };
+                    $scope.getStatus = function (index) {
+                        var result = '&#8987;';
+                        if ($scope.progressValues[index] > 0) {
+                            result = $scope.progressValues[index] + "%";
+                        }
+                        if ($scope.progressValues[index] >= 100) {
+                            result = '&#10004;';
+                        }
+                        return result;
+                    };
+                };
+
+                return {
+                    controller: [
+                        '$scope',
+                        multiStepProgress
+                    ],
+                    scope: {
+                        steps: '=steps',
+                        progressValues: '='
+                    },
+                    templateUrl: 'multistepprogress.html',
+                    replace: true
+                };
+            }
+        ])
+        .filter('to_trusted', ['$sce', function ($sce) {
+            return function (text) {
+                return $sce.trustAsHtml(text);
             };
-        }
-    ]);
+        }]);
+}());
